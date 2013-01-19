2d1ddbf33ebd95acec7d8f4d0a9b329ecbe71940
Aquietzero <zhaoyunhaosss@gmail.com>
2013-01-19 16:26:24 +0800

    mod: index style.

--- app/webroot/js/zero.js
+++ app/webroot/js/zero.js
@@ -1,11 +1,36 @@
 $(function() {
 
-  var $container = $('#cascade-container');
-
-  $container.imagesLoaded( function(){
-    $container.masonry({
-      itemSelector : '.image-block'
+    var $container = $('#cascade-container');
+    $container.imagesLoaded( function(){
+        $container.masonry({
+            itemSelector : '.image-block'
+        });
     });
-  });
+
+    $(window).scroll(function() {
+        var scrollTop = $(window).scrollTop();
+
+        var nav_h = $('#navigator').height();
+        var p_h = $('#navigator p').height();
+        var margin = parseInt($('#navigator').css('margin-top'));
+        var offset = (nav_h - p_h) - 20 + margin;
+
+        if (scrollTop >= offset) {
+            $('#header').css({
+                position: 'fixed',
+                top: -offset + 'px',
+                borderBottom: 'solid 1px #ddd',
+                boxShadow: '0 0 10px #ddd'
+            });
+            $('#container').css('marginTop', offset + 20 + 'px');
+        } else {
+            $('#header').css({ 
+                position: 'static',
+                border: 'none',
+                boxShadow: 'none'
+            });
+            $('#container').css('marginTop', '0px');
+        }
+    });    
 
 });
